ff1d567cedcea402eeca4d3ad94df8a260b81d9e
nihal-ns <nihalns2002@gmail.com>
2026-01-22 09:40:24 +0530

    updated the sequence

--- src/sequence/dma_io_addr_seq.sv
+++ src/sequence/dma_io_addr_seq.sv
@@ -13,24 +13,33 @@ class io_addr_seq extends uvm_sequence;
     
 		w_data = $urandom();
 
+		`uvm_info(get_type_name()," <------------ IO_ADDR SEQUENCE STARTED ----------->\n ", UVM_MEDIUM) 	
+
 		`uvm_info(get_type_name(), $sformatf(" IO_ADDR[31:0] = %0d", w_data), UVM_MEDIUM) 
-		`uvm_info(get_type_name(), $sformatf("Writing IO_ADDR = %0d", w_data), UVM_MEDIUM) 
-		regbk.io_addr.write( status, w_data,.parent(this) ); 
+		`uvm_info(get_type_name(), $sformatf("Writing IO_ADDR = %0d\n", w_data), UVM_MEDIUM) 
+		regbk.io_addr.write(status, w_data, UVM_FRONTDOOR ); 
 
-		if (status != UVM_IS_OK) `uvm_error(get_type_name(), "IO_ADDR register write failed") 
+		if (status != UVM_IS_OK) 
+			`uvm_error(get_type_name(), "IO_ADDR register write failed\n") 
 		
-		mirror = regbk.io_addr.get_mirrored_value(); 
-		`uvm_info(get_type_name(), $sformatf("IO_ADDR mirrored = %0d", mirror), UVM_MEDIUM)
+		/* mirror = regbk.io_addr.get_mirrored_value(); */ 
+		/* `uvm_info(get_type_name(), $sformatf("IO_ADDR mirrored = %0d\n", mirror), UVM_MEDIUM) */
 
-		regbk.io_addr.read( status, r_data,.parent(this) ); 
-		`uvm_info(get_type_name(), $sformatf("Read IO_ADDR = %0d", r_data), UVM_MEDIUM) 
+		regbk.io_addr.read(status, r_data ); 
+		`uvm_info(get_type_name(), $sformatf("Read IO_ADDR = %0d\n", r_data), UVM_MEDIUM) 
 
-		if (status != UVM_IS_OK) `uvm_error(get_type_name(), "IO_ADDR register read failed") 
+		if (status != UVM_IS_OK) 
+			`uvm_error(get_type_name(), "IO_ADDR register read failed\n") 
 		
-		if (r_data != w_data) `uvm_error(get_type_name(), "io_mem mismatch") 
-		else `uvm_info(get_type_name(),"IO_ADDR register contents passed",UVM_NONE)
+		if (r_data != w_data) 
+			`uvm_error(get_type_name(), "io_addr mismatch") 
+		else 
+			`uvm_info(get_type_name(),"IO_ADDR register contents passed",UVM_NONE)
 
-		`uvm_info(get_type_name(), $sformatf(" IO_ADDR[31:0] = %0d" , r_data), UVM_MEDIUM) 
+		`uvm_info(get_type_name(), $sformatf(" IO_ADDR[31:0] = %0d\n" , r_data), UVM_MEDIUM) 
+	
+		`uvm_info(get_type_name()," <------------ IO_ADDR SEQUENCE ENDED ----------->\n ", UVM_MEDIUM) 	
+
 	endtask
 
 endclass
--- src/sequence/dma_mem_addr_seq.sv
+++ src/sequence/dma_mem_addr_seq.sv
@@ -7,30 +7,38 @@ class mem_addr_seq extends uvm_sequence;
 	endfunction 
 
 	task body; 
-		uvm_status_e status; 
-		bit [31:0] w_data ,r_data; 
+		uvm_status_e status;
+		bit [31:0] w_data ,r_data;
 		bit [31:0] mirror;
-    
+
 		w_data = $urandom();
+    
+		`uvm_info(get_type_name()," <------------ MEM_ADDR SEQUENCE STARTED ----------->\n ", UVM_MEDIUM)
 
-		`uvm_info(get_type_name(), $sformatf(" MEM_ADDR[31:0] = %0d", w_data), UVM_MEDIUM) 
-		`uvm_info(get_type_name(), $sformatf(" Writing MEM_ADDR = %0d", w_data), UVM_MEDIUM) 
-		regbk.mem_addr.write( status, w_data,.parent(this) ); 
+		`uvm_info(get_type_name(), $sformatf(" MEM_ADDR[31:0] = %0d", w_data), UVM_MEDIUM)
+		`uvm_info(get_type_name(), $sformatf(" Writing MEM_ADDR = %0d\n", w_data), UVM_MEDIUM)
+		regbk.mem_addr.write(status, w_data);
 
-		if (status != UVM_IS_OK) `uvm_error(get_type_name(), "MEM_ADDR register write failed") 
-		
-		mirror = regbk.mem_addr.get_mirrored_value(); 
-		`uvm_info(get_type_name(), $sformatf("MEM_ADDR mirrored = %0d", mirror), UVM_MEDIUM)
+		if (status != UVM_IS_OK) 
+			`uvm_error(get_type_name(), "MEM_ADDR register write failed\n")
 
-		regbk.mem_addr.read( status, r_data,.parent(this) ); 
-		`uvm_info(get_type_name(), $sformatf("Read MEM_ADDR = %0d", r_data), UVM_MEDIUM) 
+		/* mirror = regbk.mem_addr.get_mirrored_value(); */
+		/* `uvm_info(get_type_name(), $sformatf("MEM_ADDR mirrored = %0d\n", mirror), UVM_MEDIUM) */
 
-		if (status != UVM_IS_OK) `uvm_error(get_type_name(), "MEM_ADDR register read failed") 
-		
-		if (r_data != w_data) `uvm_error(get_type_name(), "mem_addr mismatch") 
-		else `uvm_info(get_type_name(),"MEM_ADDR register contents passed",UVM_NONE)
+		regbk.mem_addr.read( status, r_data);
+		`uvm_info(get_type_name(), $sformatf("Read MEM_ADDR = %0d\n", r_data), UVM_MEDIUM)
 
-		`uvm_info(get_type_name(), $sformatf(" MEM_ADDR[31:0] = %0d" , r_data), UVM_MEDIUM) 
+		if (status != UVM_IS_OK) 
+			`uvm_error(get_type_name(), "MEM_ADDR register read failed\n")
+
+		if (r_data != w_data) 
+			`uvm_error(get_type_name(), "mem_addr mismatch\n")
+		else 
+			`uvm_info(get_type_name(),"MEM_ADDR register contents passed",UVM_NONE)
+
+		`uvm_info(get_type_name(), $sformatf(" MEM_ADDR[31:0] = %0d\n" , r_data), UVM_MEDIUM)
+
+		`uvm_info(get_type_name()," <------------ MEM_ADDR SEQUENCE ENDED ----------->\n ", UVM_MEDIUM)
 	endtask
 
 endclass
