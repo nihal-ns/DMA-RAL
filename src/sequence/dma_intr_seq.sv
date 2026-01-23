a330bd32a105e844c2815b328bb0bb37eb10879f
nihal-ns <nihalns2002@gmail.com>
2026-01-23 14:03:08 +0530

    fixed the display

--- src/sequence/dma_intr_seq.sv
+++ src/sequence/dma_intr_seq.sv
@@ -9,21 +9,20 @@ class intr_seq extends uvm_sequence;
 	task body; 
 		uvm_status_e status; 
 		bit [31:0] w_data ,r_data, prev_data; 
-		bit [31:0] mirror;
-		w_data[15:0] = 16'h2323; 
-		w_data[31:16] = 16'hABFF; 
+		w_data[15:0] = $random; 
+		w_data[31:16] = $random; 
 
 		`uvm_info(get_type_name()," <------------ INTR SEQUENCE STARTED -----------> \n", UVM_MEDIUM) 	
 
 		`uvm_info(get_type_name(), $sformatf(" INTR[15:0] = %0d | INTR[31:16] = %0d ", w_data[15:0], w_data[31:16] ), UVM_MEDIUM) 
 		`uvm_info(get_type_name(),"Writing w_data to the dut INTR register \n", UVM_MEDIUM) 
 		/* regbk.intr.read(status, prev_data, UVM_BACKDOOR); */ 
-		/* regbk.intr.write(status, w_data, UVM_BACKDOOR); */ 
+		regbk.intr.write(status, $random(), UVM_FRONTDOOR); // to skip the reset stage
 		regbk.intr.poke(status, w_data); 
 		prev_data = w_data;
 		
-		w_data[15:0] = 16'h1331; 
-		w_data[31:16] = 16'hAABB; 
+		w_data[15:0] = $random; 
+		w_data[31:16] = $random; 
 
 		regbk.intr.write(status, w_data); 
 		`uvm_info(get_type_name(), $sformatf(" INTR[15:0] = %0d | INTR[31:16] = %0d ", w_data[15:0], w_data[31:16] ), UVM_MEDIUM) 
@@ -42,30 +41,17 @@ class intr_seq extends uvm_sequence;
 			`uvm_error(get_type_name(), "INTR register read failed\n") 
 
 		if (prev_data[15:0] == r_data[15:0]) 
-			`uvm_info(get_type_name(), "intr_reg status is read only \n", UVM_NONE) 
+			`uvm_info(get_type_name(), "intr_reg status is RO \n", UVM_NONE) 
 		else 
-			`uvm_error(get_type_name(),"intr_reg status is read write \n")
+			`uvm_error(get_type_name(),"intr_reg status is RW \n")
 
 		if (prev_data[31:16] == r_data[31:16]) 
-			`uvm_error(get_type_name(), "intr_reg mask is read only \n") 
+			`uvm_error(get_type_name(), "intr_reg mask is RO \n") 
 		else 
-			`uvm_info(get_type_name(),"intr_reg mask is read write \n",UVM_NONE)
+			`uvm_info(get_type_name(),"intr_reg mask is RW \n",UVM_NONE)
 
 		`uvm_info( get_type_name(), $sformatf( "INTR[15:0] = %0d | INTR[31:16] = %0d\n", r_data[15:0], r_data[31:16] ), UVM_MEDIUM)
-
 		`uvm_info(get_type_name()," <------------ INTR SEQUENCE ENDED ----------->\n ", UVM_MEDIUM) 
-
-		/* uvm_status_e status; */
-		/* bit [31:0] w_data, r_data, prev_data; */
-		/* regbk.intr.read(status, prev_data, UVM_BACKDOOR); */
-		
-		/* w_data = $urandom_range(1,30); */
-		/* regbk.intr.write(status, w_data, UVM_BACKDOOR); */
-		/* regbk.intr.read(status, r_data); */
-		/* if(prev_data == r_data) */
-		/* 	$display("Read only"); */
-		/* else */
-		/* 	$display("Read and write"); */
 	endtask
 
 endclass
