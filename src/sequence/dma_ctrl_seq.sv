57ff597b555caccb7ba6ecc7128e34c87b256f49
nihal-ns <nihalns2002@gmail.com>
2026-01-23 14:35:22 +0530

    fixed the display level

--- src/sequence/dma_ctrl_seq.sv
+++ src/sequence/dma_ctrl_seq.sv
@@ -11,27 +11,51 @@ class ctrl_seq extends uvm_sequence;
 		bit [31:0] w_data ,r_data; 
 		bit [31:0] mirror;
 		w_data[0] = 1; 
-		w_data[15:1] = 15'd16; 
+		w_data[15:1] = $random; 
 		w_data[16] = 1; 
+
+		if(m_sequencer.get_report_verbosity_level() >= UVM_MEDIUM)
+			$display("\n |--------------------------------- CTRL SEQUENCE STARTED --------------------------------|\n ");
 
 		`uvm_info(get_type_name(), $sformatf(" CTRL[0] = %0d | CTRL[15:1] = %0d | CTRL[16] = %0d", w_data[0], w_data[15:1] ,w_data[16]), UVM_MEDIUM) 
 		`uvm_info(get_type_name(), $sformatf("Writing CTRL = %0d", w_data), UVM_MEDIUM) 
-		regbk.ctrl.write( status, w_data,.parent(this) ); 
+		regbk.ctrl.write(status, w_data); // to skip the initial reset 
+		regbk.ctrl.write(status, w_data); 
 
-    if (status != UVM_IS_OK) `uvm_error(get_type_name(), "CTRL register write failed") 
-
-		mirror = regbk.ctrl.get_mirrored_value(); 
-		`uvm_info(get_type_name(), $sformatf("CTRL mirrored = %0d", mirror), UVM_MEDIUM)
+    if (status != UVM_IS_OK) 
+			`uvm_error(get_type_name(), "CTRL register write failed") 
 
 		regbk.ctrl.read( status, r_data,.parent(this) ); 
 		`uvm_info(get_type_name(), $sformatf("Read CTRL = %0d", r_data), UVM_MEDIUM) 
 
-		if (status != UVM_IS_OK) `uvm_error(get_type_name(), "CTRL register read failed") 
+		if (status != UVM_IS_OK) 
+			`uvm_error(get_type_name(), "CTRL register read failed") 
 
-		if (r_data[0] != 1 && r_data[15:1] != 15'd16 && r_data[16] != 1) `uvm_error(get_type_name(), "ctrl_mem mismatch") 
-		else `uvm_info(get_type_name(),"CTRL register contents passed",UVM_NONE)
+	// Display values
+		$display("*****************************************CHECK*****************************************");
+		$display("Field\t\t   Write Value\tRead Value");
+		$display("ctrl_start_dma     \t%0b\t   %0b",w_data[0], r_data[0]);
+		$display("ctrl_w_count\t    %0d\t   %0d",w_data[15:1], r_data[15:1]);
+		$display("ctrl_io_mem     \t%0b\t   %0b",w_data[16], r_data[16]);
+		
+		if (r_data[0] == 0) 
+			`uvm_info(get_type_name(),"ctrl_start_dma is RW",UVM_NONE)
+		else 
+			`uvm_error(get_type_name(), "ctrl_start_dma is RO") 
 
+		if (r_data[15:1] == w_data[15:1]) 
+			`uvm_info(get_type_name(),"ctrl_w_count is RW",UVM_NONE)
+		else 
+			`uvm_error(get_type_name(), "ctrl_w_count is RO") 
+
+		if (r_data[0] == 0) 
+			`uvm_info(get_type_name(),"ctrl_start_dma is RO",UVM_NONE)
+		else 
+			`uvm_error(get_type_name(), "ctrl_start_dma is RW") 
 		`uvm_info(get_type_name(), $sformatf(" CTRL[0] = %0d | CTRL[15:1] = %0d | CTRL[16] = %0d", r_data[0], r_data[15:1] ,r_data[16]), UVM_MEDIUM) 
+
+		if(m_sequencer.get_report_verbosity_level() >= UVM_MEDIUM)
+			$display("\n |------------------------------------ CTRL SEQUENCE ENDED --------------------------------|\n");
 	endtask
 
 endclass
--- src/sequence/dma_intr_seq.sv
+++ src/sequence/dma_intr_seq.sv
@@ -12,9 +12,10 @@ class intr_seq extends uvm_sequence;
 		w_data[15:0] = $random; 
 		w_data[31:16] = $random; 
 
-		`uvm_info(get_type_name()," <------------ INTR SEQUENCE STARTED -----------> \n", UVM_MEDIUM) 	
+		if(m_sequencer.get_report_verbosity_level() >= UVM_MEDIUM)
+			$display("\n |---------------------------------------- INTR SEQUENCE STARTED ------------------------------| \n");
 
-		`uvm_info(get_type_name(), $sformatf(" INTR[15:0] = %0d | INTR[31:16] = %0d ", w_data[15:0], w_data[31:16] ), UVM_MEDIUM) 
+		`uvm_info(get_type_name(), $sformatf(" INTR[15:0] = %0d | INTR[31:16] = %0d ", w_data[15:0], w_data[31:16]), UVM_MEDIUM) 
 		`uvm_info(get_type_name(),"Writing w_data to the dut INTR register \n", UVM_MEDIUM) 
 		/* regbk.intr.read(status, prev_data, UVM_BACKDOOR); */ 
 		regbk.intr.write(status, $random(), UVM_FRONTDOOR); // to skip the reset stage
@@ -40,18 +41,24 @@ class intr_seq extends uvm_sequence;
 		if (status != UVM_IS_OK) 
 			`uvm_error(get_type_name(), "INTR register read failed\n") 
 
+		// Display values
+		$display("*****************************************CHECK*****************************************");
+		$display("Field\t\t   Write Value\tRead Value");
+		$display("intr_status\t     %0d\t    %0d",w_data[15:0], r_data[15:0]);
+		$display("intr_mask\t     %0d\t    %0d",w_data[31:16], r_data[31:16]);
+
 		if (prev_data[15:0] == r_data[15:0]) 
-			`uvm_info(get_type_name(), "intr_reg status is RO \n", UVM_NONE) 
+			`uvm_info(get_type_name(), "intr_reg status is RO ", UVM_LOW) 
 		else 
-			`uvm_error(get_type_name(),"intr_reg status is RW \n")
+			`uvm_error(get_type_name(),"intr_reg status is RW ")
 
 		if (prev_data[31:16] == r_data[31:16]) 
-			`uvm_error(get_type_name(), "intr_reg mask is RO \n") 
+			`uvm_error(get_type_name(), "intr_reg mask is RO ") 
 		else 
-			`uvm_info(get_type_name(),"intr_reg mask is RW \n",UVM_NONE)
+			`uvm_info(get_type_name(),"intr_reg mask is RW ",UVM_LOW)
 
-		`uvm_info( get_type_name(), $sformatf( "INTR[15:0] = %0d | INTR[31:16] = %0d\n", r_data[15:0], r_data[31:16] ), UVM_MEDIUM)
-		`uvm_info(get_type_name()," <------------ INTR SEQUENCE ENDED ----------->\n ", UVM_MEDIUM) 
+		if(m_sequencer.get_report_verbosity_level() >= UVM_MEDIUM)
+			$display("\n|--------------------------------- INTR SEQUENCE ENDED ----------------------------------|\n ");
 	endtask
 
 endclass
